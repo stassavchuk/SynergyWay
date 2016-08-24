3176599719c7334e8398c7d2ea689e198293aa8b
Stanislav Savchuk <stanislav.savchuk@gmail.com>
2016-08-24 18:42:31 +0300

    Add urls

--- src/users/urls.py
+++ src/users/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='user_list'),
+    url(r'^$', views.users, name='users'),
+    url(r'^create/$', views.create, name='create'),
+    url(r'^edit/(?P<user_id>[0-9]+)$', views.edit, name='edit'),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world")
+def users(request):
+    template = loader.get_template('users.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def create(request):
+    template = loader.get_template('create.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def edit(request, user_id):
+    template = loader.get_template('edit.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
