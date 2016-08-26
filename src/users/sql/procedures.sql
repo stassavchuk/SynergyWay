b8db8016bc7ad04e69f8cdc040c183a08bf46e10
Stanislav Savchuk <stanislav.savchuk@gmail.com>
2016-08-26 14:52:17 +0300

    Add procedure to create DataBase

--- src/users/sql/procedures.sql
+++ src/users/sql/procedures.sql
@@ -191,3 +191,71 @@ CREATE OR REPLACE FUNCTION update_records(u_id INTEGER, course_list INTEGER[])
   $func$ LANGUAGE plpgsql;
 
 SELECT update_records(5, array[1]);
+
+
+-------------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------
+--                        CREATING DATABASE/TABLES                      --
+--------------------------------------------------------------------------
+
+-- Important extension
+CREATE EXTENSION dblink;
+
+CREATE OR REPLACE FUNCTION create_db(db_name VARCHAR(32))
+  RETURNS void AS
+  $func$
+      BEGIN
+        IF exists(SELECT 1 FROM pg_database WHERE datname = db_name) THEN
+        ELSE
+          PERFORM dblink_exec('dbname=' || current_database()   -- current db
+                     , 'CREATE DATABASE ' || quote_ident(db_name));
+        END IF;
+      END;
+  $func$ LANGUAGE plpgsql;
+
+
+SELECT create_db('newDB');
+
+----------------------------------------------------------
+CREATE OR REPLACE FUNCTION create_db_with_tables(db_name VARCHAR(32))
+  RETURNS void AS
+  $func$
+      BEGIN
+        IF exists(SELECT 1 FROM pg_database WHERE datname = db_name) THEN
+        ELSE
+          PERFORM dblink_exec('dbname=' || current_database()   -- current db
+                     , 'CREATE DATABASE ' || quote_ident(db_name));
+
+          PERFORM dblink_exec('dbname=' || db_name,
+            ' -- Table "user"
+              CREATE TABLE IF NOT EXISTS "users" (
+                  user_id     SERIAL        PRIMARY KEY,
+                  user_name   VARCHAR (64)  NOT NULL,
+                  email       VARCHAR (128) NOT NULL,
+                  status      BOOLEAN       NOT NULL      DEFAULT FALSE,
+                  phone       VARCHAR (16),
+                  m_phone     VARCHAR (16)
+              );
+
+              -- Table "courses"
+              CREATE TABLE IF NOT EXISTS "courses" (
+                  course_id   SERIAL        PRIMARY KEY,
+                  course_name VARCHAR (64)  NOT NULL,
+                  code        VARCHAR (8)   NOT NULL
+              );
+
+              -- Table "records", the table aimed to connect user and courses as many-to-many
+              CREATE TABLE IF NOT EXISTS "records" (
+                  rec_id      SERIAL        PRIMARY KEY,
+                  user_id     INTEGER       NOT NULL      REFERENCES users (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+                  course_id   INTEGER       NOT NULL      REFERENCES courses (course_id) ON UPDATE CASCADE ON DELETE CASCADE
+              );'
+          );
+        END IF;
+      END;
+  $func$ LANGUAGE plpgsql;
+
+SELECT create_db_with_tables('dbWithTables');
+
+------------------------------------------------------------------------------
